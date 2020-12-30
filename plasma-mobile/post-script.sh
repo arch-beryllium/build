33466822690e97eefde3250ae6e34e1bef4672d5
jld3103 <jld3103yt@gmail.com>
2020-12-30 15:27:17 +0100

    Increase scaling setting timeout

--- plasma-mobile/post-script.sh
+++ plasma-mobile/post-script.sh
@@ -10,7 +10,7 @@ cp -r /etc/xdg/. /home/alarm/.config/
 mkdir -p /home/alarm/.config/autostart-scripts
 cat >>/home/alarm/.config/autostart-scripts/initial-scale.sh <<EOF
 #!/bin/bash
-sleep 5s
+sleep 10s
 export QT_QPA_PLATFORM=wayland
 kscreen-doctor output.1.scale.2.5
 rm /home/alarm/.config/autostart-scripts/initial-scale.sh
