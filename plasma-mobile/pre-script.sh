f04a6e531ffad21dfb95dc6023fe2b5a6b6d4950
jld3103 <jld3103yt@gmail.com>
2021-01-05 21:16:37 +0100

    Only enable phosh repo on phosh

--- plasma-mobile/pre-script.sh
+++ plasma-mobile/pre-script.sh
@@ -1,3 +1,1 @@
-sed -i "s/#\[plasma-mobile\]/\[plasma-mobile\]/" /etc/pacman.conf
-sed -i "s/#Server = https:\/\/repo.lohl1kohl.de\/plasma-mobile\/aarch64\//Server = https:\/\/repo.lohl1kohl.de\/plasma-mobile\/aarch64\//" /etc/pacman.conf
-sed -i "s/#Include = \/etc\/pacman.d\/mirrorlist/Include = \/etc\/pacman.d\/mirrorlist/" /etc/pacman.conf
+sed -i "s/#plasma-mobile: //" /etc/pacman.conf
